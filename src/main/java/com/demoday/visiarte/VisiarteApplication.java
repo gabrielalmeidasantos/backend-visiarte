b40baa21b9be6d419c29ed989dba32e5e60789ab
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2022-12-07 19:51:18 -0300

    inicio backend projeto

---
+++ src/main/java/com/demoday/visiarte/VisiarteApplication.java
@@ -0,0 +1,13 @@
+package com.demoday.visiarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisiarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisiarteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoday/visiarte/VisiarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoday.visiarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisiarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
